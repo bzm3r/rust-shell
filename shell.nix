0471f2123018f0e2082fe493b8112792cbe3194e
Brian Merchant <bzm3r@proton.me>
2023-12-01 11:12:43 -0800

    errors due to lack of attention

--- shell.nix
+++ shell.nix
@@ -38,16 +38,15 @@ pkgs.mkShell.override {
       cargoToml = pkgs.writeText "cargo.toml"
       ''
         [build]
-        rustc-wrapper = "${pkgs.sccache}"
+        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
       '';
   in ''
     pwd
     export nixpkgs=${sources.nixpkgs.outPath}
     export NIX_PATH=nixpkgs=${sources.nixpkgs.outPath}
     mkdir .cargo
-    cp --remove-destination ${cargoToml} ./.cargo/Cargo.toml
+    cp --remove-destination ${cargoToml} ./.cargo/config.toml
     mkdir .sccache
     export SCCACHE_DIR="$(realpath ./.sccache)"
-    export RUSTC_WRAPPER="$(realpath ./.sccache)"
   '';
 }
