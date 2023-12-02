8ef3a6cf18684b404ebc75fafd0d662763e81556
Brian Merchant <bzm3r@proton.me>
2023-12-01 16:31:45 -0800

    not interested in direnv

--- shell.nix
+++ shell.nix
@@ -6,47 +6,57 @@ let
     ];
   };
 in
-pkgs.mkShell.override {
+pkgs.mkShell.override
+{
   stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
 }
 {
-  name = "rust-template";
-
-  # is this actually necessary here?
-  buildInputs = with pkgs; [
-    npins
-  ];
+  name = "rust_stable";
 
   packages = with pkgs; [
     (
       rust-bin.stable.latest.default.override
-        {
-          extensions = [
-            "rustfmt"
-            "rust-std"
-            "rust-src"
-            "rust-analyzer"
-            "clippy"
-            "llvm-tools-preview"
-          ];
-        }
+      {
+        extensions = [
+          "rustfmt"
+          "rust-std"
+          "rust-src"
+          "rust-analyzer"
+          "clippy"
+          "llvm-tools-preview"
+        ];
+      }
     )
     sccache
+    npins
   ];
 
-  shellHook = let
-      cargoToml = pkgs.writeText "cargo.toml"
-      ''
-        [build]
-        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
-      '';
-  in ''
-    pwd
-    export nixpkgs=${sources.nixpkgs.outPath}
-    export NIX_PATH=nixpkgs=${sources.nixpkgs.outPath}
-    mkdir .cargo
-    cp --remove-destination ${cargoToml} ./.cargo/config.toml
-    mkdir .sccache
-    export SCCACHE_DIR="$(realpath ./.sccache)"
-  '';
+  shellHook =
+    let
+      cargoConf = pkgs.writeText "cargo.toml"
+        ''
+          [build]
+          rustc-wrapper = "${pkgs.sccache}/bin/sccache"
+        '';
+    in
+    ''
+      # export variables
+      export nixpkgs=${sources.nixpkgs.outPath}
+      export NIX_PATH=nixpkgs=${sources.nixpkgs.outPath}
+
+      # make a cargo directory (if it doesn't already exist)
+      mkdir .cargo
+
+      # overwrite any ./.cargo/config.toml that exists
+      cp --remove-destination ${cargoConf} ./.cargo/config.toml
+
+      # create .sccache dir (if it doesn't already exist)
+      mkdir .sccache
+
+      # let sccache know where we want to store data
+      export SCCACHE_DIR="$(realpath ./.sccache)"
+
+      # # use zsh, not bash (nix-shell starts bash)
+      # exec zsh
+    '';
 }
