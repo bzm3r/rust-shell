e5fa502d2b7fbcd84caea817d746f64f148bc49c
Brian Merchant <bzm3r@proton.me>
2023-12-05 21:38:14 -0800

    restore shell.nix

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ let
   name = "rust-stable";
   cacheDir = "~/.${name}_sccache";
   cargoConfigDir = "~/.cargo_${name}/";
-  cargoConfigToml = cargoConfigDir + "/config.toml";
+  cargoConfigPath = cargoConfigDir + "/config.toml";
 in
 pkgs.mkShell.override
 {
@@ -32,17 +32,36 @@ pkgs.mkShell.override
       }
     )
     sccache
-    (callPackage )
   ];
+
+  shell = "/usr/bin/zsh";
 
   shellHook =
     let
-      cargoConfigToml = pkgs.writeText "config.toml"
+      storedCargoConfig = pkgs.writeText "config.toml"
         ''
           [build]
           rustc-wrapper = "${pkgs.sccache}/bin/sccache"
         '';
+      nixpkgsOutPath = sources.nixpkgs.outPath;
     in
     ''
+      # export variables
+      export nixpkgs=${nixpkgsOutPath}
+      export NIX_PATH=nixpkgs=${nixpkgsOutPath}
+
+      # make a .cargo directory (if it doesn't already exist)
+      echo "Creating CARGO_HOME at ${cargoConfigDir}"
+      mkdir ${cargoConfigDir}
+      export CARGO_HOME=${cargoConfigDir}
+      # overwrite any existing config.toml
+      cp --remove-destination ${storedCargoConfig} ${cargoConfigPath}
+
+      # create .<name>_sccache cargoConfigDir (if it doesn't already exist)
+      echo "Creating SCCACHE_DIR at ${cacheDir}"
+      mkdir ${cacheDir}
+      SCCACHE_DIR=$(realpath ${cacheDir})
+      export SCCACHE_DIR=$SCCACHE_DIR
+      export DEFAULT_WORKSPACE=${name}
     '';
 }
