ae7931724cbb447adf2f0a6ffa4247831f23bf6a
Brian Merchant <bzm3r@proton.me>
2023-12-11 21:06:07 -0800

    should output binaries into the bin folder so that they are properly symlinked

--- mkDevShell.nix
+++ mkDevShell.nix
@@ -2,13 +2,13 @@
 # mkDevShell is closely based on the structure of mkShell
 # * https://stackoverflow.com/a/71112117/3486684
 {
-  # ===========================================
-  # mkShell Attributes:
-  # https://nixos.org/manual/nixpkgs/unstable/#sec-pkgs-mkShell-attributes
-  # ===========================================
-  #
-  # List of executable packages to add to the nix-shell environment.
-  packages ? [ ]
+# ===========================================
+# mkShell Attributes:
+# https://nixos.org/manual/nixpkgs/unstable/#sec-pkgs-mkShell-attributes
+# ===========================================
+#
+# List of executable packages to add to the nix-shell environment.
+packages ? [ ]
   #
   # -----------------------
   #
@@ -25,59 +25,55 @@
   #
   # Set the name of the derivation. (Not optional)
 , name
-  # -----------------------
-  # Many packages have dependencies that are not provided in the standard
-  # environment. It’s usually sufficient to specify those dependencies in the
-  # buildInputs attribute.
-  #
-  # This attribute ensures that the bin subare listed here:https://nixos.org/manual/nix/unstable/language/derivations.html#derivations
-  # * [3] https://nixos.org/guides/nix-pills/fundamentals-of-stdenv#id1463
-  #
-  # A derivation has a "builder": a script which executes building the
-  # derviation. This builder invokes a variety of "phases".
-  #
-  # Phases can be overridden by setting the envVar/attribute namePhase to a
-  # string containing some shell commands to be executed (from within a
-  # derivation; note: derivations work so that any attributes that
-  # are not "used anywhere" else are turned into environment variables (see ref
-  # [2]) or by redefining the shell function namePhase (from within a shell script).
-  #
-  # Usually: only want to add some commands to a phase, and keep most of it
-  # intact. This is usually done by setting pre/post phases. The defaults for
-  # each phase can be seen here
-  # https://github.com/NixOS/nixpkgs/blob/ec6a7ec59fd4064c7b9535b9c2fe3c441197e3f7/pkgs/stdenv/generic/setup.sh
-  #
-  # (It seems to me, so far, that we should not add the phase attributes, unless
-  # we have a clear/explicit value for them, because otherwise we will end up
-  # overriding the phases?)
-  #
-  # The unpack phase is responsible for unpacking the source code of the
-  # package. The buildPhase helps execute commands necessary to "build it"
-  # (whatever that means outside of C-ish contexts), and the installPhase copies
-  # the output files to their final locations
-  #, unpackPhase, buildPhase, installPhase
-  #
-  # and though we could use the phases, we could also just define our own builder:
-  # , builder ? ""
-  # #
-  # # Add dependencies to nativeBuildInputs if they are executed during the build
-  # # process.
-  # , nativeBuildInputs ? [ ]
-  # # Add dependencies to buildInputs if they will end up copied or linked into the
-  # # final output or otherwise used at runtime.
-  # , buildInputs ? [ ]
-  # # Dependencies needed only to run tests are similarly classified between native
-  # # (executed during build) and non-native (executed at runtime). These
-  # # dependencies are only injected when doCheck is set to true.
-  # #, nativeCheckInputs, checkInputs
-  # , propagatedBuildInputs ? [ ]
-  # , propagatedNativeBuildInputs ? [ ]
-  # The remaining attributes will all be converted to into environment variables
-, CARGO_HOME
-, SCCACHE_DIR
-, storedCargoConfig
-, ...
-}@inputAttrs:
+# -----------------------
+# Many packages have dependencies that are not provided in the standard
+# environment. It’s usually sufficient to specify those dependencies in the
+# buildInputs attribute.
+#
+# This attribute ensures that the bin subare listed here:https://nixos.org/manual/nix/unstable/language/derivations.html#derivations
+# * [3] https://nixos.org/guides/nix-pills/fundamentals-of-stdenv#id1463
+#
+# A derivation has a "builder": a script which executes building the
+# derviation. This builder invokes a variety of "phases".
+#
+# Phases can be overridden by setting the envVar/attribute namePhase to a
+# string containing some shell commands to be executed (from within a
+# derivation; note: derivations work so that any attributes that
+# are not "used anywhere" else are turned into environment variables (see ref
+# [2]) or by redefining the shell function namePhase (from within a shell script).
+#
+# Usually: only want to add some commands to a phase, and keep most of it
+# intact. This is usually done by setting pre/post phases. The defaults for
+# each phase can be seen here
+# https://github.com/NixOS/nixpkgs/blob/ec6a7ec59fd4064c7b9535b9c2fe3c441197e3f7/pkgs/stdenv/generic/setup.sh
+#
+# (It seems to me, so far, that we should not add the phase attributes, unless
+# we have a clear/explicit value for them, because otherwise we will end up
+# overriding the phases?)
+#
+# The unpack phase is responsible for unpacking the source code of the
+# package. The buildPhase helps execute commands necessary to "build it"
+# (whatever that means outside of C-ish contexts), and the installPhase copies
+# the output files to their final locations
+#, unpackPhase, buildPhase, installPhase
+#
+# and though we could use the phases, we could also just define our own builder:
+# , builder ? ""
+# #
+# # Add dependencies to nativeBuildInputs if they are executed during the build
+# # process.
+# , nativeBuildInputs ? [ ]
+# # Add dependencies to buildInputs if they will end up copied or linked into the
+# # final output or otherwise used at runtime.
+# , buildInputs ? [ ]
+# # Dependencies needed only to run tests are similarly classified between native
+# # (executed during build) and non-native (executed at runtime). These
+# # dependencies are only injected when doCheck is set to true.
+# #, nativeCheckInputs, checkInputs
+# , propagatedBuildInputs ? [ ]
+# , propagatedNativeBuildInputs ? [ ]
+# The remaining attributes will all be converted to into environment variables
+, CARGO_HOME, SCCACHE_DIR, storedCargoConfig, ... }@inputAttrs:
 let
   # deduplicates builtInputs across: 1) any corresponding buildInputs in
   # inputAttrs (the inputs to this `mkDevShell` function), 2) all the
@@ -87,21 +83,17 @@ let
     # check if there is an attribute with the same name as `focusSet` in this
     # `mkDevShell's` input attributes; if so, get it, otherwise begin with the
     # empty list
-    (inputAttrs.${buildInputs} or [ ]) ++
-    (
+    (inputAttrs.${buildInputs} or [ ]) ++ (
       # lib.subtractLists: subtracts first list from second
       # https://nixos.org/manual/nixpkgs/unstable/
       lib.subtractLists
-        # first list is a list derivations whose build inputs will be included
-        # in the final dev shell's environment.
-        inputsFrom
-        (
-          # flattens nested lists into an un-nested one
-          lib.flatten
-            # remove all buildInputs that are already listed in inputsFrom
-            (lib.catAttrs buildInputs inputsFrom)
-        )
-    );
+      # first list is a list derivations whose build inputs will be included
+      # in the final dev shell's environment.
+      inputsFrom (
+        # flattens nested lists into an un-nested one
+        lib.flatten
+        # remove all buildInputs that are already listed in inputsFrom
+        (lib.catAttrs buildInputs inputsFrom)));
 
   # remove from `attrs` the listed attributes
   rest = builtins.removeAttrs inputAttrs [
@@ -116,12 +108,11 @@ let
     "shellHook"
   ];
   CARGO_CONFIG_PATH = CARGO_HOME + "/config.toml";
-in
-# (From:
+  # (From:
   #   * https://nixos.org/manual/nixpkgs/unstable/#sec-using-stdenv
   #   * https://nixos.org/manual/nixpkgs/unstable/#ssec-stdenv-attributes
   # )
-stdenv.mkDerivation ({
+in stdenv.mkDerivation ({
   inherit name;
   # =================================================================
   # (From: https://nixos.org/manual/nixpkgs/unstable/#chap-cross)
@@ -153,66 +144,60 @@ stdenv.mkDerivation ({
 
   phases = [ "buildPhase" "installPhase" ];
 
-  buildPhase =
-    let
-      # a concatenation of the various shell hookzs that are required by
-      # the buildInputs that go into making up the shell environment
-      shellHooks = lib.concatStringsSep "\n" (
-        lib.catAttrs "shellHook"
-          (
-            lib.reverseList inputsFrom ++ [ inputAttrs ]
-          )
-      );
-      mkDirs = ''
-        mkdir -p "${CARGO_HOME}"
-        # TODO: in the future, perform a merge with an existing file?
-        cp --remove-destination  ${storedCargoConfig} ${CARGO_CONFIG_PATH}
-        mkdir -p "${SCCACHE_DIR}"
-      '';
-      shellInitContent = ''
-        # shell hooks concatenated from build inputs for this shell
-        ${shellHooks}
-        # make cargo home directory, sccache directory, and config.toml
-        ${mkDirs}
-      '';
-      shellInit = "shell-init";
-      exportFixes = import ./exportFixes.nix { inherit lib shellInit; };
-      startShell = ''
-        #!/usr/bin/env zsh
-        source $out/shell-init ; zsh -i
-      '';
-    in
-    ''
-      runHook preBuild
-      # echo "buildPhase PWD: $PWD"
-      # echo "buildPhase out: $out"
-      # echo "buildPhase ls: $(ls)"
-
-      echo "exporting vars to shellInit"
-      export >> ${shellInit}
-      ${exportFixes}
-
-      echo "${shellInitContent}" >> ${shellInit}
-      echo "${startShell}" >> ${name}
-      runHook postBuild
+  buildPhase = let
+    # a concatenation of the various shell hookzs that are required by
+    # the buildInputs that go into making up the shell environment
+    shellHooks = lib.concatStringsSep "\n"
+      (lib.catAttrs "shellHook" (lib.reverseList inputsFrom ++ [ inputAttrs ]));
+    mkDirs = ''
+      mkdir -p "${CARGO_HOME}"
+      # TODO: in the future, perform a merge with an existing file?
+      cp --remove-destination  ${storedCargoConfig} ${CARGO_CONFIG_PATH}
+      mkdir -p "${SCCACHE_DIR}"
     '';
-
-  installPhase =
-    ''
-      runHook preInstall
-      # echo "PWD: $PWD"
-      # echo "out: $out"
-      # echo "ls: $(ls)"
-      install -m 755 -D --target-directory $out $PWD/shell-init
-      install -m 755 -D --target-directory $out $PWD/${name}
-      runHook postInstall
+    shellInitContent = ''
+      # shell hooks concatenated from build inputs for this shell
+      ${shellHooks}
+      # make cargo home directory, sccache directory, and config.toml
+      ${mkDirs}
     '';
+    shellInit = "shell-init";
+    exportFixes = import ./exportFixes.nix { inherit lib shellInit; };
+    startShell = ''
+      #!/usr/bin/env zsh
+      source $out/shell-init ; zsh -i
+    '';
+  in ''
+    runHook preBuild
+    # echo "buildPhase PWD: $PWD"
+    # echo "buildPhase out: $out"
+    # echo "buildPhase ls: $(ls)"
+
+    echo "exporting vars to shellInit"
+    export >> ${shellInit}
+    ${exportFixes}
+
+    echo "${shellInitContent}" >> ${shellInit}
+    echo "${startShell}" >> ${name}
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    # echo "PWD: $PWD"
+    # echo "out: $out"
+    # echo "ls: $(ls)"
+    install -m 755 -D --target-directory $out/bin $PWD/shell-init
+    install -m 755 -D --target-directory $out/bin $PWD/${name}
+    runHook postInstall
+  '';
 
   preferLocalBuild = true;
 
   meta = with lib; {
     #homepage = "xyz";
-    description = "Rust development shell for integration with IDEs and personal experimentation. This is not meant to be an environment within which builds meant for distribution are produced.";
+    description =
+      "Rust development shell for integration with IDEs and personal experimentation. This is not meant to be an environment within which builds meant for distribution are produced.";
     #license = licenses.ofl;
     platforms = platforms.all;
     maintainers = [ ];
