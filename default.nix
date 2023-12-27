bf72305424380edefad5d4c10c186ee63e422fa3
Brian Merchant <bzm3r@proton.me>
2023-12-26 22:58:18 -0800

    wip

--- default.nix
+++ default.nix
@@ -1,25 +1,22 @@
-# pkgs could from the derivation, or outside of it
-{ description, base ? "~/CACHE", pkgs ? (import <nixpkgs> { }), lib, ... }:
-assert (let shellFamily = "rust";
-in lib.assertMsg (description != "rust")
-"Description cannot be same as shell family name (${shellFamily})!");
+let shellFamily = "rust";
+in { pkgs ? (import <nixpkgs> { }), lib, description, base ? "~/CACHE", ... }:
+assert lib.assertMsg (description != "rust"
+  "Description cannot be same as shell family name (${shellFamily})!");
 let
-  mkDevShell = pkgs.callPackage ((import ./npins).mkDevShell);
+  mkDevShell = (import ./npins).mkDevShell;
   stdenv = (pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv);
-  mkdirs = rec {
+  mkdirs = {
     SHELL_BASE = "${base}/rust/${description}";
     CARGO_HOME = "${base}/rust/${description}";
     SCCACHE_DIR = "${base}/sccache/${description}";
   };
-  mkfiles = rec {
-    configToml = {
-      text = ''
-        [build]
-        rustc-wrapper = "${pkgs.sccache}/bin/sccache"
-      '';
-      path = "${mkdirs.CARGO_HOME}/config.toml";
-    };
-  };
+  mkfiles = [{
+    text = ''
+      [build]
+      rustc-wrapper = "${pkgs.sccache}/bin/sccache"
+    '';
+    path = "${mkdirs.CARGO_HOME}/config.toml";
+  }];
   packages = with pkgs; [
     rustfmt
     rustc
@@ -32,12 +29,17 @@ let
   ];
 
   meta = {
-    #homepage = "xyz";
     description =
       "Rust development shell for integration with IDEs and personal experimentation. This is not meant to be an environment within which builds meant for distribution are produced.";
-    #license = licenses.ofl;
     platforms = pkgs.lib.platforms.all;
     maintainers = [ ];
     mainProgram = description;
   };
-in mkDevShell { } { inherit description stdenv mkdirs mkfiles packages meta; }
+in mkDevShell {
+  shellName = {
+    family = shellFamily;
+    inherit description;
+  };
+
+  inherit pkgs lib stdenv mkdirs mkfiles packages meta;
+}
